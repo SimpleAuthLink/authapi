ef317f46d78c49002450a1125443609cf76d6c1f
Lucas Menendez <hi@lucasmenendez.me>
2026-07-05 22:53:10 +0200

    refactor(cmd): adapt entrypoints to new system and update config
    Update authapi and demo entrypoints to use the generic notification
    queue and new email config types. Update example.env, docker files,
    and gitignore to reflect the new configuration layout.

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -2,62 +2,118 @@ package main
 
 import (
 	"context"
-	"log"
+	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/simpleauthlink/authapi/api"
-	"github.com/simpleauthlink/authapi/cmd"
-	"github.com/simpleauthlink/authapi/internal/osflag"
+	"github.com/simpleauthlink/authapi/notification"
 	"github.com/simpleauthlink/authapi/notification/email"
+	"github.com/simpleauthlink/authapi/notification/email/templates/login"
+	"go.k7z7z.cc/x/flag"
+	"go.k7z7z.cc/x/log"
+	"go.k7z7z.cc/x/net/http/io"
+	"go.k7z7z.cc/x/net/smtp/testsmtp"
+	"go.k7z7z.cc/x/proc"
 )
 
+type DemoConfig struct {
+	APIHost string `env:"HOST" flag:"host" sflag:"h" usage:"service host"`
+	APIPort int    `env:"PORT" flag:"port" sflag:"p" usage:"service port"`
+	Secret  string `env:"SECRET" flag:"secret" sflag:"s" usage:"secret used to generate the tokens"`
+}
+
+var demoMailInbox = make(chan string, 1)
+
 func main() {
-	var (
-		demoServer string
-		demoPort   int
-		demoSecret string
-	)
-	osflag.StringVar(&demoServer, cmd.HostEnv, cmd.HostFlag, cmd.DefaultHost, cmd.HostFlagDesc, false)
-	osflag.IntVar(&demoPort, cmd.PortEnv, cmd.PortFlag, cmd.DefaultPort, cmd.PortFlagDesc, false)
-	osflag.StringVar(&demoSecret, cmd.SecretEnv, cmd.SecretFlag, cmd.DefaultSecret, cmd.SecretFlagDesc, false)
-	if err := osflag.Parse(nil); err != nil {
-		log.Fatalln("ERR: error parsing flags:", err)
+	log.SetLevel(log.DebugLevel)
+	c := &DemoConfig{
+		APIHost: "0.0.0.0",
+		APIPort: 8080,
+		Secret:  "demo-service",
 	}
-	log.Println("INF: starting service with config:", demoServer, demoPort, demoSecret)
+	if err := flag.UnmarshalArgs(os.Args[1:], c); err != nil {
+		log.Errw("error parsing flags", "err", err)
+		return
+	}
+	log.Infow("config loaded", "config", c)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	// create the email queue
-	emailQueue, err := email.NewEmailQueue(context.Background(), &email.EmailConfig{
-		FromName:    "SimpleAuthLink Demo",
+	demoMailServer := testsmtp.NewServer(c.APIHost, 2525, demoMailInbox)
+	if err := demoMailServer.Start(ctx); err != nil {
+		log.Errw("error starting test smtp server", "err", err)
+		return
+	}
+	emailConfig := &email.EmailConfig{
 		FromAddress: "demo@simpleauth.link",
-		SMTPServer:  demoServer,
+		SMTPServer:  c.APIHost,
 		SMTPPort:    2525,
-	})
+	}
+	// create the email queue
+	notificationQueue, err := notification.NewQueue(ctx, 10, emailConfig)
 	if err != nil {
-		log.Fatalln("WRN: something occurs during email queue creation:", err)
+		log.Errw("error initializing notification queue", "err", err)
+		return
 	}
 	// start the email queue and defer to stop it
-	emailQueue.Start()
-	defer emailQueue.Stop()
-	// create the service
-	service, err := api.New(ctx, &api.Config{
-		Server:       demoServer,
-		ServerPort:   demoPort,
-		Secret:       demoSecret,
-		DemoMode:     true,
-		DemoSMTPAddr: demoServer,
-		DemoSMTPPort: 2525,
-	}, emailQueue)
+	notificationQueue.Start(5)
+	defer notificationQueue.Stop()
+	// create the apiService
+	apiService, err := api.New(ctx, &api.Config{
+		Server:     c.APIHost,
+		ServerPort: c.APIPort,
+		Secret:     c.Secret,
+	}, notificationQueue)
 	if err != nil {
-		log.Fatalln("ERR: error creating service:", err)
+		log.Errw("error creating api service", "err", err)
+		return
 	}
-	// start the service in background
-	go func() {
-		if err := service.Start(); err != nil {
-			log.Fatalln("ERR: error running service:", err)
+	if err := apiService.Get("/demo/inbox", demoInboxHandler); err != nil {
+		log.Errw("error adding new handler for inbox", "err", err)
+		return
+	}
+	// start the service and wait for it to finish
+	if err = new(proc.Shutdown).WithInterrupt().WithRecovery().WithRun(func(ctx context.Context) error {
+		return apiService.Start()
+	}).WithCallback(func(_ context.Context) error {
+		return apiService.Stop()
+	}).Wait(ctx); err != nil {
+		log.Errw("error during shutdown", "err", err)
+		return
+	}
+	log.Info("HTTP server stopped, exiting...")
+}
+
+func demoInboxHandler(w http.ResponseWriter, r *http.Request) {
+	// get the email from get parameters
+	email := r.URL.Query().Get("email")
+	if email == "" {
+		io.NewAPIError(400001, http.StatusBadRequest).Write(w)
+		return
+	}
+	// set http headers required for SSE
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	// create a channel for client disconnection
+	clientGone := r.Context().Done()
+	// create a response controller
+	rc := http.NewResponseController(w)
+	for {
+		select {
+		case <-clientGone:
+			return
+		case msg := <-demoMailInbox:
+			// find the token in the email
+			if testToken := login.FindToken(email, msg); testToken != nil {
+				// send an event to the client with the token in the "data" field
+				if _, err := fmt.Fprintf(w, "data: %s\n\n", testToken); err != nil {
+					return
+				}
+				if err := rc.Flush(); err != nil {
+					return
+				}
+			}
 		}
-	}()
-	// wait for the service to finish
-	if err := service.WaitToShutdown(); err != nil {
-		log.Fatalln("ERR: error waiting for service to finish:", err)
 	}
 }
